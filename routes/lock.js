9316e750f9c30d15815e144a46db718e31ec4f32
Arno Hollosi <arno.hollosi@campus02.at>
2017-12-16 12:01:00 +0100

    Lock-Service: PUT-Route zum Aktualisieren (lock/unlock)

--- routes/lock.js
+++ routes/lock.js
@@ -11,4 +11,24 @@ function readLock(req, res) {
     res.json(lock);
 }
 
+// PUT-Route (PUT, weil Status aktualisiert wird)
+router.put('/', updateLock);
+
+function updateLock(request, response) {
+    // Parameter auslesen
+    let newstate = request.body.lockValue;
+
+    // False -> True // True -> False
+    if (newstate !== lock) {
+        lock = newstate;
+        console.log("Lock-Update: " + lock);
+        response.json("success");
+    }
+    // False -> False // True -> True
+    else {
+        console.log("Fehler bei Lock-Update");
+        response.json("error");
+    }
+}
+
 module.exports = router;
